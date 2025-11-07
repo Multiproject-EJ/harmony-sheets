8abe860dfe67eb345e6f531b2328b542b21810ee
Multiproject-EJ <josefsen.eivind@gmail.com>
2025-11-07 00:28:28 +0000

    Revert "Add multi-domain Supabase auth modal"

--- assets/supaClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-import { getSiteConfig } from "./site-config.js";
-
-const cfg = getSiteConfig();
-export const REDIRECT_TO = cfg.redirectTo;
-export const supabase = createClient(cfg.supabaseUrl, cfg.anonKey);
